3e3b754e50573717ca4cab4ac1f9ea173fb93986
Polash Mahmud <polashmahmud@gmail.com>
2023-07-22 20:58:16 +0600

    login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex justify-center">
+        <div class="w-4/12 bg-white p-6 rounded-lg">
+            @if(session('status'))
+                <div class="bg-red-500 text-white p-4 rounded-lg mb-6 text-center">
+                    {{ session('status')  }}
+                </div>
+            @endif
+
+            <form action="{{ route('login') }}" method="POST">
+                @csrf
+
+                <div class="mb-4">
+                    <label for="email" class="sr-only">Email</label>
+                    <input type="email" name="email" id="email" value="{{ old('email') }}" placeholder="Your email"
+                           class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('email') border-red-500 @enderror">
+                    @error('email')
+                    <div class="text-red-500 text-sm mt-2">
+                        {{ $message }}
+                    </div>
+                    @enderror
+                </div>
+                <div class="mb-4">
+                    <label for="password" class="sr-only">Password</label>
+                    <input type="password" name="password" id="password" placeholder="Your password"
+                           class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('password') border-red-500 @enderror">
+                    @error('password')
+                    <div class="text-red-500 text-sm mt-2">
+                        {{ $message }}
+                    </div>
+                    @enderror
+                </div>
+
+                <div>
+                    <button class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full" type="submit">Login
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
@@ -21,3 +22,6 @@ Route::get('/dashboard', DashboardController::class)->name('dashboard');
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
